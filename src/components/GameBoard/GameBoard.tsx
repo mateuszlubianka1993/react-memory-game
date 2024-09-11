e81d261d6e30c6542d39d415460e1906f4616586
mateusz-lubianka-lingaro <mateusz.lubianka@lingarogroup.com>
2024-09-11 11:53:27 +0200

    Create GameBoard and GameCard components

---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,82 @@
+import { FC, useState, useEffect } from "react";
+import { GameCard } from "../../components";
+import { createDeck } from "../../helpers/createDeck";
+import { DeckItem, GameBoardProps, GameCardMode } from '../../types';
+import { ROUND_TIME } from "../../config/game";
+import styles from "./gameBoard.module.scss";
+
+const GameBoard: FC<GameBoardProps> = ({userName}) => {
+    const [openPairs, setOpenPairs] = useState<string[]>([]);
+    const [foundPairs, setFoundPairs] = useState<string[]>([]);
+    const [cards, setCards] = useState<DeckItem[]>([]);
+    const [disabled, setDisabled] = useState(false);
+    const endGame = foundPairs.length === cards.length / 2;
+    const handleCardClick = (id: string) => {
+        setOpenPairs(prevState => [...prevState, id]);
+    };
+    const isCardOpen = (id: string) => {
+        const found = openPairs.find(openId => openId === id) || foundPairs.find(foundId =>  id.includes(foundId));
+
+        return !!found;
+    };
+
+    const checkPairs = () => {
+        if (openPairs.length === 2) {
+            const [first, second] = openPairs;
+            const firstPairId = first.split('-')[0];
+            const secondPairId = second.split('-')[0];
+
+            if (firstPairId === secondPairId) {
+                setFoundPairs(precState => [...precState, firstPairId]);
+                setOpenPairs([]);
+                setDisabled(false);
+            } else {
+                setTimeout(() => {
+                    setOpenPairs([]);
+                    setDisabled(false);
+                }, ROUND_TIME);
+            }
+        }
+    };
+    
+    useEffect(() => {
+        const cards = createDeck(GameCardMode.FLAGS);
+
+        setCards(cards);
+    }, []);
+
+    useEffect(() => {
+        if (openPairs.length === 2) {
+            setDisabled(true);
+        }
+        checkPairs();
+    }, [openPairs]);
+
+    if (endGame) {
+        return (
+            <div className={styles.root}>
+                <h2 className={styles.root__title}>Congratulations {userName}!</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div className={styles.root}>
+            <h2 className={styles.root__title}>Hello {userName}!</h2>
+            <div className={styles.root__boardContainer}>
+                {cards.map((card, index) => (
+                    <GameCard
+                        key={`${card.pairId}-${index}`}
+                        card={card}
+                        onClick={() => handleCardClick(card.id)}
+                        blockClickOpen={true}
+                        disabled={disabled}
+                        isOpen={isCardOpen(card.id)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default GameBoard;
--- src/types/game.types.ts
+++ src/types/game.types.ts
@@ -1,3 +1,27 @@
 export interface GameCreatorProps {
     onGameCreated: (userName: string) => void;
 }
+
+export interface GameBoardProps {
+    userName: string;
+}
+
+export enum GameCardMode {
+    FLAGS = "FLAGS",
+}
+
+export interface DeckItem {
+    pairId: string;
+    img: string;
+    name: string;
+    id: string;
+}
+
+export interface GameCardProps {
+    card: DeckItem;
+    onClick: () => void;
+    mode?: GameCardMode;
+    isOpen: boolean;
+    blockClickOpen?: boolean;
+    disabled?: boolean;
+}
